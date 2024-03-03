69e1b6c7d6b6fd06b461a7eb0520b2b3bf7be952
aldara <aldarafernandezcarro@gmail.com>
2024-03-03 12:21:43 +0100

    Se crea el mapper para el carrito

--- src/main/java/daw/entornoservidor/model/DTO/mapper/DTOMapperCart.java
+++ src/main/java/daw/entornoservidor/model/DTO/mapper/DTOMapperCart.java
@@ -1,5 +1,32 @@
 package daw.entornoservidor.model.DTO.mapper;
 
+import daw.entornoservidor.model.User;
+import daw.entornoservidor.model.DTO.UserCartDTO;
+
+
 public class DTOMapperCart {
 
+	private User user;
+
+	
+	
+	private DTOMapperCart() {
+	}
+	
+	
+	public static DTOMapperCart builder() {
+		return new DTOMapperCart();
+	}
+
+	public DTOMapperCart setUser (User user) {
+		this.user = user;
+		return this;
+	}
+	
+	public UserCartDTO build() {
+		if (user == null) {
+			throw new RuntimeException();
+		}
+		return new UserCartDTO(user.getId(), user.getUsername(), user.getCart());
+	}
 }
